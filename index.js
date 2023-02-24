7219a5f6f5058e064f7ecb7d7368b609c8d0c0c7
Ilja M <i.meleshkevich@gmail.com>
2023-02-25 00:50:11 +0100

    refactor with sessionStorage

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 /*Please include index.css in HTML code for using this.*/
+
 const parentDiv = document.getElementById("parent");
+const messageDiv = document.getElementById("message");
+
+const setClickedPiece = (params) => {
+  sessionStorage.setItem("row", `${params.row}`);
+  sessionStorage.setItem("column", `${params.column}`);
+  sessionStorage.setItem("pieceClass", `${params.pieceClass}`);
+};
 
 const createPiece = (square, cssClass) => {
   const piece = document.createElement("div");
   piece.setAttribute("class", `piece-${cssClass}`);
+
   if (3 > square.getAttribute("row") || 6 < square.getAttribute("row")) {
     square.appendChild(piece);
+    piece.addEventListener("click", (e) => {
+      e.stopPropagation();
+      piece.classList.add("red-border");
+      const pieceParent = piece.parentNode;
+
+      const pieceParentRow = pieceParent.getAttribute("row");
+      const pieceParentColumn = pieceParent.getAttribute("column");
+      const params = {
+        row: pieceParentRow,
+        column: pieceParentColumn,
+        pieceClass: `piece-${cssClass}`,
+      };
+
+      setClickedPiece(params);
+    });
   }
 };
-
 const newBoard = () => {
   const chessBoard = document.createElement("div");
   chessBoard.setAttribute("id", "chessBoard");
   parentDiv.appendChild(chessBoard);
-
-  // board
 
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
@@ -30,10 +51,19 @@ const newBoard = () => {
         chessSquare.classList.add("white");
         createPiece(chessSquare, "white");
       }
-
+      chessSquare.addEventListener("click", () => {
+        chessSquare.classList.add("red-border");
+        //TODO separate func for this logic
+        if (chessSquare.getAttribute("row") < sessionStorage.getItem("row")) {
+          messageDiv.innerHTML = "Move not allowed";
+        }
+      });
       chessBoard.appendChild(chessSquare);
     }
   }
-  console.log("window loaded");
 };
-window.onload = newBoard();
+const init = () => {
+  sessionStorage.clear();
+  newBoard();
+};
+window.onload = init();
